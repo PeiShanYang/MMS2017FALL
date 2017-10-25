4e584e8009de57109a74447fb8c3f14b88e905f6
PeiShanYang <32910139+PeiShanYang@users.noreply.github.com>
2017-10-26 01:30:32 +0800

    Update Assignment1.cpp
    將灰階的圖改成只有一通道

--- Assignment1/Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1/Assignment1.cpp
@@ -14,84 +14,80 @@ using namespace cv;
 int main()
 {
 	Mat Image = imread("000.jpg");
-	Mat GrayImage = Image.clone(); // Gray
+	Mat GrayImage(500, 500, CV_8UC1); // Gray
 	Mat BW1Image(1000, 1000, CV_8UC1); // Dithering
 	Mat BW2Image(500, 500, CV_8UC1); // Side Effect of Dithering
 	
 	int i, j;
-	uchar g; // g是用來放三個顏色的平均
 
 	// 轉成灰階
 	for(i = 0; i < Image.cols; i++)
 	{
 		for(j = 0; j < Image.rows; j++)
 		{
-			g = (Image.at<Vec3b>(i, j)[0] + Image.at<Vec3b>(i, j)[1] + Image.at<Vec3b>(i, j)[2]) / 3;
-			//if(i == 250)printf("(%d, %d) %u\n", i, j, g);
-			GrayImage.at<Vec3b>(i, j)[0] = g;
-			GrayImage.at<Vec3b>(i, j)[1] = g;
-			GrayImage.at<Vec3b>(i, j)[2] = g;
+			GrayImage.at<uchar>(i, j) = (Image.at<Vec3b>(i, j)[0] + Image.at<Vec3b>(i, j)[1] + Image.at<Vec3b>(i, j)[2]) / 3;
 		}
-	}
+	} // gray = (Green + Red + Blue) / 3
 
 	// 黑白Dithering
-	for (i = 0; i < GrayImage.cols; i++)
+	for(i = 0; i < GrayImage.cols; i++)
 	{
-		for (j = 0; j < GrayImage.rows; j++)
+		for(j = 0; j < GrayImage.rows; j++)
 		{
-			if (GrayImage.at<Vec3b>(i, j)[0] >= 201)
+			if(GrayImage.at<uchar>(i, j) >= 201)
 			{
 				BW1Image.at<uchar>(2 * i, 2 * j) = 255;
 				BW1Image.at<uchar>(2 * i, 2 * j + 1) = 255;
 				BW1Image.at<uchar>(2 * i + 1, 2 * j) = 255;
 				BW1Image.at<uchar>(2 * i + 1, 2 * j + 1) = 255;
-			}
-			else if (GrayImage.at<Vec3b>(i, j)[0] >= 151)
+			} // 全白
+			else if(GrayImage.at<uchar>(i, j) >= 151)
 			{
 				BW1Image.at<uchar>(2 * i, 2 * j) = 0;
 				BW1Image.at<uchar>(2 * i, 2 * j + 1) = 255;
 				BW1Image.at<uchar>(2 * i + 1, 2 * j) = 255;
 				BW1Image.at<uchar>(2 * i + 1, 2 * j + 1) = 255;
-			}
-			else if (GrayImage.at<Vec3b>(i, j)[0] >= 101)
+			} // 點黑第0格
+			else if(GrayImage.at<uchar>(i, j) >= 101)
 			{
 				BW1Image.at<uchar>(2 * i, 2 * j) = 0;
 				BW1Image.at<uchar>(2 * i, 2 * j + 1) = 255;
 				BW1Image.at<uchar>(2 * i + 1, 2 * j) = 255;
 				BW1Image.at<uchar>(2 * i + 1, 2 * j + 1) = 0;
-			}
-			else if (GrayImage.at<Vec3b>(i, j)[0] >= 51)
+			} // 點黑第0、3格
+			else if(GrayImage.at<uchar>(i, j) >= 51)
 			{
 				BW1Image.at<uchar>(2 * i, 2 * j) = 0;
 				BW1Image.at<uchar>(2 * i, 2 * j + 1) = 0;
 				BW1Image.at<uchar>(2 * i + 1, 2 * j) = 255;
 				BW1Image.at<uchar>(2 * i + 1, 2 * j + 1) = 0;
-			}
+			} // 點黑第0、1、3格
 			else
 			{
 				BW1Image.at<uchar>(2 * i, 2 * j) = 0;
 				BW1Image.at<uchar>(2 * i, 2 * j + 1) = 0;
 				BW1Image.at<uchar>(2 * i + 1, 2 * j) = 0;
 				BW1Image.at<uchar>(2 * i + 1, 2 * j + 1) = 0;
-			}
+			} // 全黑
 		}
 	}
 
 	// 黑白Side Effect of Dithering
 	int DitherMatrix[16] = {0, 8, 2, 10, 12, 4, 14, 6, 3, 11, 1, 9, 15, 7, 13, 5};
 	int x, y;
-	for (i = 0; i < GrayImage.cols; i++)
+	for(i = 0; i < GrayImage.cols; i++)
 	{
-		for (j = 0; j < GrayImage.rows; j++)
+		for(j = 0; j < GrayImage.rows; j++)
 		{
 			x = i % 4;
 			y = j % 4;
-			if (GrayImage.at<Vec3b>(i, j)[0]> DitherMatrix[x * 4 + y] * 16)
+			if(GrayImage.at<uchar>(i, j) > DitherMatrix[x * 4 + y] * 16)
 				BW2Image.at<uchar>(i ,j) = 255;
 			else BW2Image.at<uchar>(i, j) = 0;
 		}
 	}
-	/* BEGIN
+	/* Algo.
+	BEGIN
 		for x = 0 to Xmax // columns
 			for y = 0 to Ymax // rows
 				i = x mod n;
@@ -102,13 +98,13 @@ int main()
 					O(x, y) = 1;
 				else
 					O(x, y) = 0;
-		END */
+	END */
 	
 
 	namedWindow("Image", WINDOW_AUTOSIZE);
-	namedWindow("GrayImage", WINDOW_AUTOSIZE);
-	namedWindow("BW1Image", WINDOW_AUTOSIZE);
-	namedWindow("BW2Image", WINDOW_AUTOSIZE);
+	namedWindow("Gray", WINDOW_AUTOSIZE);
+	namedWindow("Dithering", WINDOW_AUTOSIZE);
+	namedWindow("Side Effect of Dithering", WINDOW_AUTOSIZE);
 	imshow("Image", Image);
 	imshow("Gray", GrayImage);
 	imshow("Dithering", BW1Image);
